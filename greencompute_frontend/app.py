9973a4dba5cdbff1925394403e8991f15ebf8e2d
Elias Tavarez <etav@umich.edu>
2024-09-27 18:32:33 -0500

    feat: try connecting to api in cluster

--- greencompute_frontend/app.py
+++ greencompute_frontend/app.py
@@ -1,3 +1,10 @@
 import streamlit as st
+import requests
 
 st.title("GreenCompute")
+
+# Get the data from the API
+response = requests.get("http://localhost:8000/health")
+
+if st.button("Check the health of the API"):
+    st.write(response.json())

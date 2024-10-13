93f33271f86001f7726a53069073c56c00678668
Elias Tavarez <etav@umich.edu>
2024-10-13 16:50:34 -0500

    feat: add first working version of chat UI

---
+++ greencompute_frontend/pages/chat.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import requests
+
+st.title("GreenCompute Recommender")
+
+
+def chat_response(query: str):
+    # Make a post request to localhost:8000/chat with the query
+    response = requests.post("http://localhost:8000/llm/rag", json={"query": query})
+    return response.json()["body"]
+
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# React to user input
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    response = chat_response(prompt)
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})

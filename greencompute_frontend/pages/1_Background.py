221734d9f2247a2dd66313b6e2ab29add2ecb013
Elias Tavarez <111210402+etavrz@users.noreply.github.com>
2024-10-15 18:28:49 -0500

    feat: add new version of frontend with predictions/background/about us (#3)
    * feat: add reworked frontend
    
    * fix: remove data.
    
    * fix: remove model
    
    * feat: add backend calls
    
    ---------
    
    Co-authored-by: Elias Tavarez <etav@umich.edu>

--- greencompute_frontend/pages/1_Background.py
+++ greencompute_frontend/pages/1_Background.py
@@ -1,19 +1,20 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 import altair as alt
 import base64
+import requests
 
 # Paths to the logos
 logo1 = "./images/logo1.png"
 logo2 = "./images/logo2.png"
-logo3 = "./images/logo3.png" 
+logo3 = "./images/logo3.png"
+
 
 def add_logo(logo, width):
     # Read the image and convert it to Base64
     with open(logo, "rb") as f:
         data = base64.b64encode(f.read()).decode("utf-8")
-    
+
     # Inject CSS with Base64-encoded image into the sidebar
     st.markdown(
         f"""
@@ -30,15 +31,20 @@ def add_logo(logo, width):
         unsafe_allow_html=True,
     )
 
+
 # Call the add_logo function with the path to your local image
 add_logo(logo1, "260px")
 add_logo(logo3, "260px")
 
 
-df = pd.read_csv("./Cloud Carbon Footprint - Embodied Emissions.csv")
+try:
+    data = requests.get("http://localhost:8000/ml/emissions-data").json()
+    df = pd.DataFrame(data)
+except requests.exceptions.RequestException:
+    df = pd.read_csv("./Cloud Carbon Footprint - Embodied Emissions.csv")
 
 # Rename columns
-df.columns = ['series', 'vm', 'CPU', 'memory', 'carbon_emission', 'carbon_emission2']
+df.columns = ["series", "vm", "CPU", "memory", "carbon_emission", "carbon_emission2"]
 
 # Change the background color
 st.markdown(
@@ -49,14 +55,16 @@ st.markdown(
     }
     </style>
     """,
-    unsafe_allow_html=True
+    unsafe_allow_html=True,
 )
 
 # Customizing the title with HTML/CSS to make it larger and green
 st.markdown("<h1 style='color: green;'>Background</h1>", unsafe_allow_html=True)
 
 # Overview
-st.write("##### Data centers are critical infrastructure that significantly contribute to global carbon emissions. Operators lack effective tools to track, manage, and optimize their carbon footprint.")
+st.write(
+    "##### Data centers are critical infrastructure that significantly contribute to global carbon emissions. Operators lack effective tools to track, manage, and optimize their carbon footprint."
+)
 
 # Create two columns (left and right)
 col1, col2 = st.columns(2)
@@ -66,15 +74,15 @@ with col1:
     st.write("##### Carbon Emission vs. # of CPUs")
     scatter_plot = (
         alt.Chart(df)
-        .mark_circle(color='orange')  # Set the color to green
+        .mark_circle(color="orange")  # Set the color to green
         .encode(
-            x='CPU',
-            y='carbon_emission',
-            tooltip=['CPU','carbon_emission']  # Optional: show values on hover
+            x="CPU",
+            y="carbon_emission",
+            tooltip=["CPU", "carbon_emission"],  # Optional: show values on hover
         )
         .properties(width=400, height=300)  # Set plot size
     )
-    
+
     st.altair_chart(scatter_plot, use_container_width=True)
 
 # Scatter plot for Column C vs Column B
@@ -82,14 +90,13 @@ with col2:
     st.write("##### Carbon Emission vs. Memory")
     scatter_plot = (
         alt.Chart(df)
-        .mark_circle(color='blue')  # Set the color to green
+        .mark_circle(color="blue")  # Set the color to green
         .encode(
-            x='memory',
-            y='carbon_emission',
-            tooltip=['memory', 'carbon_emission']  # Optional: show values on hover
+            x="memory",
+            y="carbon_emission",
+            tooltip=["memory", "carbon_emission"],  # Optional: show values on hover
         )
         .properties(width=400, height=300)  # Set plot size
     )
-    
-    st.altair_chart(scatter_plot, use_container_width=True)
 
+    st.altair_chart(scatter_plot, use_container_width=True)
--- greencompute_frontend/pages/2_About_Us.py
+++ greencompute_frontend/pages/2_About_Us.py
@@ -1,6 +1,4 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
 import base64
 
 # Paths to the logos
@@ -8,11 +6,12 @@ logo1 = "./images/logo1.png"
 logo2 = "./images/logo2.png"
 logo3 = "./images/logo3.png"
 
+
 def add_logo(logo, width):
     # Read the image and convert it to Base64
     with open(logo, "rb") as f:
         data = base64.b64encode(f.read()).decode("utf-8")
-    
+
     # Inject CSS with Base64-encoded image into the sidebar
     st.markdown(
         f"""
@@ -29,6 +28,7 @@ def add_logo(logo, width):
         unsafe_allow_html=True,
     )
 
+
 # Call the add_logo function with the path to your local image
 add_logo(logo1, "260px")
 add_logo(logo3, "260px")
@@ -42,7 +42,7 @@ st.markdown(
     }
     </style>
     """,
-    unsafe_allow_html=True
+    unsafe_allow_html=True,
 )
 
 # Customizing the title with HTML/CSS to make it larger and green
@@ -55,37 +55,37 @@ members = [
         "name": "Victor Brew",
         "role": "EDA, Model Development",
         "bio": "bio...",
-        "image": "./profile_images/victor.png", 
-        "email":"vbrew@berkeley.edu"
+        "image": "./images/victor.png",
+        "email": "vbrew@berkeley.edu",
     },
     {
         "name": "Nat Buisson",
         "role": "Project Management, UI/UX",
         "bio": "bio...",
-        "image": "./profile_images/nat.jpeg",
-        "email":"nbuisson@berkeley.edu"
+        "image": "./images/nat.jpeg",
+        "email": "nbuisson@berkeley.edu",
     },
     {
         "name": "Alex Hubbard",
         "role": "SME, Model Development",
         "bio": "bio...",
-        "image": "./profile_images/alex.png",
-        "email":"alex.hubbard@berkeley.edu"
+        "image": "./images/alex.png",
+        "email": "alex.hubbard@berkeley.edu",
     },
     {
         "name": "Elias Tavarez",
         "role": "ML Engineering, GenAI",
         "bio": "bio...",
-        "image": "./profile_images/elias.png",
-        "email":"etav@berkeley.edu"
+        "image": "./images/elias.png",
+        "email": "etav@berkeley.edu",
     },
     {
         "name": "Ruiyu Zhou",
         "role": "EDA, Model Development",
         "bio": "bio...",
-        "image": "./profile_images/ruiyu.jpeg",
-        "email":"rzhou9@berkeley.edu"
-    }
+        "image": "./images/ruiyu.jpeg",
+        "email": "rzhou9@berkeley.edu",
+    },
 ]
 
 # Set up the About Us section
@@ -95,14 +95,14 @@ st.write("")
 for member in members:
     # Create a two-column layout: image on the left, bio/role on the right
     col1, col2 = st.columns([1, 2])  # Adjust column width as needed
-    
+
     with col1:
         st.image(member["image"], width=150)  # Display member's image
-    
+
     with col2:
         st.subheader(member["name"])
         st.write(f"**Role:** {member['role']}")
         st.write(f"**Bio:** {member['bio']}")
         st.write(f"**Email Address:** {member['email']}")
-    
+
     st.write("---")  # Add a horizontal divider between members
